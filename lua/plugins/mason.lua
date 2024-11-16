d5b8bd012a1457010516c82090746958624694a4
Jericho Bermas <jbermas@jlabs.team>
2024-11-16 08:01:47 +0800

    feat: updated lazyvim + plugins

---
+++ lua/plugins/mason.lua
@@ -0,0 +1,16 @@
+return {
+  "williamboman/mason.nvim",
+  opts = {
+    ensure_installed = {
+      "css-lsp",
+      "eslint-lsp",
+      "intelephense",
+      "lua-language-server",
+      "prettier",
+      "pretty-php",
+      "shfmt",
+      "some-sass-language-server",
+      "stylua",
+    },
+  },
+}
